53480856253a7ad653fcd1c85dda75afcf86b9ba
Qilei Ren <renqilei@gmail.com>
2014-11-22 02:54:43 +0000

    tick-tack for every matter you want to count down
    tick-tack for every matter you want to count down.

---
+++ TikTaker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TikTaker
+//
+//  Created by 任琦磊 on 14/11/22.
+//  Copyright (c) 2014年 任琦磊. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
